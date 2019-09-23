5f3c0e7af7eab307fab40615e375c0adf185c41d
Venkatasai <Venkatasai@DESKTOP-VUST0R7>
2019-09-23 15:24:07 +0530

    added somthing

--- gitdemo/src/com/rs/gitdemo/D.java
+++ gitdemo/src/com/rs/gitdemo/D.java
@@ -4,6 +4,9 @@ public class D {
 
 	public static void main(String[] args) {
 		System.out.println("This is D class");
+		System.out.println("iam sai");
+		System.out.println("he is calling rowdy");
+		System.out.println("somthing is better than nothing");
 	}
 
 }
