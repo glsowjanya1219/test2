01a0946de3bad7d3b2b9174ced0578b3fe1acb06
pinte <pinte@LAPTOP-K5KND2RE>
2019-09-23 15:14:12 +0530

    pintesh change

--- gitdemo/src/com/rs/gitdemo/C.java
+++ gitdemo/src/com/rs/gitdemo/C.java
@@ -3,5 +3,6 @@ package com.rs.gitdemo;
 public class C {
  public static void main(String args[]) {
 	 System.out.println("This is venkatasai");
+	 System.out.println("This is pintesh behera");
  }
 }
